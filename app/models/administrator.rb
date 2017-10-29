acfe6bb30ca3ab74d134ebab9982d0419590f8b7
Michael Hulet <michael@hulet.eu>
2017-10-29 16:20:10 +0100

    Add Administrator model and routes

---
+++ app/models/administrator.rb
@@ -0,0 +1,9 @@
+class Administrator < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :invitable, :database_authenticatable, :recoverable, :rememberable, :trackable,
+         :validatable
+
+  validates :email, presence: true
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :administrators,
+             path: 'administrators',
+             controllers: {
+               sessions: 'administrators/sessions'
+             }
+
+  namespace :admin do
+    resources :administrators
+    root to: 'administrators#index'
+  end
 end
